edc89a2357d5803346b3f1391904f96a1fc869a1
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2020-11-13 13:21:33 +0100

    updated readme, added stub interfaces to LibData
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ src/libdata/LibData.mjs
@@ -0,0 +1,25 @@
+export const start = async (redisConfig) => {
+
+};
+
+export const stop = async () => {
+
+};
+
+export const getDoctors = async ({
+  limit = null,
+  offset = null,
+  orderBy: {
+    field: null,
+  } = null,
+}) => {
+
+};
+
+export const getDoctor = async (doctorId = null) => {
+
+};
+
+export const saveDoctor = async () => {
+
+};
